14e4e0a6807f1187888fbd4ee793fb8346505329
ZK <zk_520it@163.com>
2016-07-24 16:52:08 +0800

    sync

--- ZKAddressPickerView/ZKAddressPickerView/Classes/ZKMainViewController.m
+++ ZKAddressPickerView/ZKAddressPickerView/Classes/ZKMainViewController.m
@@ -13,7 +13,8 @@
 @interface ZKMainViewController () <ZKAddressPickerViewDelegate>
 
 @property (nonatomic, strong) ZKAddressPickerView *addressPickerView;
-@property (nonatomic, strong) ZKAddressListModel *model;
+@property (nonatomic, strong) ZKAddressListModel  *model;
+@property (weak, nonatomic) IBOutlet UIButton     *addressBtn;
 
 @end
 
@@ -22,21 +23,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    [self initAddressPickerView];
 }
 
-- (void)initAddressPickerView
+#pragma mark *** Actions ***
+- (IBAction)simulateCreateAddress
 {
-    _addressPickerView = [ZKAddressPickerView pickerViewWithDelegate:self isEdit:_model?YES:NO addressListModel:_model];
+    [self setupAddressPickerWithEdit:NO];
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+- (IBAction)simulateEditAddress
 {
-    [super touchesBegan:touches withEvent:event];
-    
+    [self setupAddressPickerWithEdit:YES];
+}
+
+- (void)setupAddressPickerWithEdit:(BOOL)edit
+{
+    // 注意: picker初始化方法写在这里不够合理(造成picker弹出后瞬白现象), 正常情况下在viewDidLoad中实现. 这里为了方便演示创建和编辑地址的picker的效果, 不得已在按钮事件中实现.
+    _addressPickerView = [ZKAddressPickerView pickerViewWithDelegate:self isEdit:edit addressListModel:edit?self.model:nil];
     [_addressPickerView show];
-    
+}
+
+#pragma mark *** <ZKAddressPickerViewDelegate> ***
+- (void)addressPickerView:(ZKAddressPickerView *)addressPickerView didSelectProvince:(NSString *)province city:(NSString *)city town:(NSString *)town
+{
+    NSString *title = [NSString stringWithFormat:@"%@ %@ %@", province, city, town];
+    [_addressBtn setTitle:title forState:UIControlStateNormal];
+}
+
+#pragma mark *** Test Data ***
+- (ZKAddressListModel *)model
+{
+    if (!_model) {
+        _model = [[ZKAddressListModel alloc] init];
+        _model.province = @"河南省";
+        _model.city = @"商丘市";
+        _model.town = @"宁陵县";
+    }
+    return _model;
 }
 
 @end
